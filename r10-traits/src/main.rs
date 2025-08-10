6de7571f9f978c34ef03cbff39183fe323e4667e
Bright, EliteBook <brightboateng003@gmail.com>
2025-08-10 06:05:53 +0000

    generics + trait practice v1.01

--- r10-traits/src/main.rs
+++ r10-traits/src/main.rs
@@ -7,6 +7,15 @@ fn main() {
     let largest = get_largest(&arr);
 
     println!("The largest number in the array is: {}", largest);
+    
+    let arr2 = ['a', 'f', 'g', 'w', 'v'];
+    let largest2: Option<char>  = get_largest2(&arr2);
+
+    match largest2 {
+        None => println!("Array2 is empty"),
+        Some(b) => println!("The largest char in the array2 is: {}", b)
+    }
+    
 }
 
 fn get_largest <T: PartialOrd> (arr: &[T]) -> &T {
@@ -19,4 +28,19 @@ fn get_largest <T: PartialOrd> (arr: &[T]) -> &T {
     }
 
     large
+}
+
+fn get_largest2 <B: PartialOrd + Copy> (arr: &[B]) -> Option<B> {
+    if arr.is_empty() {
+        return None;
+    }
+    let mut large = &arr[0];
+
+    for b in arr {
+        if b > large {
+            large = b
+        }
+    }
+
+    Some(*large)
 }
